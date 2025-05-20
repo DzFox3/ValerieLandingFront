cc7221de8478d21cc3b38fca90e3f3f847a23256
DzFox3 <diazmarcos4444@gmail.com>
2025-05-19 22:12:41 -0300

    Subida de Pagina a Git

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  preguntas = [
+    {
+      pregunta: '¿QUIÉNES PUEDEN ACCEDER A LOS SEGUROS DE VBESERENI?',
+      respuesta: 'Para toda persona que viva en Estados Unidos, sin importar el estatus migratorio, que busque cobertura médica, protección financiera o planes de seguro de vida.',
+      abierta: false // La primera abierta por defecto
+    },
+    {
+      pregunta: '¿QUÉ TIPO DE SEGUROS OFRECEMOS?',
+      respuesta: 'Ofrecemos seguros médicos, de vida, financieros, etc.',
+      abierta: false
+    },
+    {
+      pregunta: '¿LOS SEGUROS TIENEN COBERTURA EN TODO ESTADOS UNIDOS?',
+      respuesta: 'Sí, la cobertura aplica en todo el territorio nacional.',
+      abierta: false
+    },
+    {
+      pregunta: '¿CUÁNTO CUESTA LA INSCRIPCIÓN A UN SEGURO?',
+      respuesta: 'La inscripción puede variar, pero tenemos opciones sin costo inicial.',
+      abierta: false
+    },
+    {
+      pregunta: '¿CÓMO PUEDO AHORRAR EN SERVICIOS MÉDICOS SIN CONTRATAR UN SEGURO?',
+      respuesta: 'Ofrecemos membresías con descuentos médicos exclusivos.',
+      abierta: false
+    }
+  ];
+
+  togglePregunta(index: number) {
+    this.preguntas[index].abierta = !this.preguntas[index].abierta;
+  }
+}
